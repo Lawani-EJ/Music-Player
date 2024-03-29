c303282b8fd8542a2b218df2cfdfbd0cbefc62f2
elyonjohn <elyonjohn60@gmail.com>
2024-03-29 09:48:40 +0100

    Variables I accesed the #next, #previous and #shuffle elements from the HTML file, Then accesed them to the variables named nextButton, previousButton and shuffleButton respectively.

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const playlistSongs = document.getElementById("playlist-songs");
 const playButton = document.getElementById("play");
 const pauseButton = document.getElementById("pause");
 
+// Took these 3 variables then dropped them here and made changes to them,
+const nextButton = document.getElementById("next");
+const previousButton = document.getElementById("previous");
+const shuffleButton = document.getElementById("shuffle");
