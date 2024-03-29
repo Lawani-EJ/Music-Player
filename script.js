a1945858f67ad1ef26c05b4d2d2ca498072c94ee
elyonjohn <elyonjohn60@gmail.com>
2024-03-29 09:40:16 +0100

    Variables Started by accessing the #playlist-songs, Then accesed the #play and #pause, elements with the getElementById() And then assigned them to the variables playlist-songs, play-button and pause-button respectively.

---
+++ script.js
@@ -0,0 +1,5 @@
+// Started off by making these variables
+const playlistSongs = document.getElementById("playlist-songs");
+const playButton = document.getElementById("play");
+const pauseButton = document.getElementById("pause");
+
