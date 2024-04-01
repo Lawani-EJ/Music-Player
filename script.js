78c11be048851948295330704dafae90af6d0b56
elyonjohn <elyonjohn60@gmail.com>
2024-04-01 11:22:16 +0100

    Map() method cont'd Inside the map() method, I added a return statement with a template literal, I created an list element withi the template literal , I gave it an id and a class

--- script.js
+++ script.js
@@ -116,6 +116,8 @@ let userData = {
 //If the function body consists of a single expression you can omit the curly braces and return keyword,
 //This is called an (implicit return). eg: const exampleArrowFunction = (param) => param;
 const renderSongs = array => {
-    const songsHTML = array.map((song) => {}); //The map() method is used to iterate through an array and returns a new array,
+    const songsHTML = array.map((song) => {
+        return `<li id="song-${song.id}" class="playlist-song"></li>`
+    }); //The map() method is used to iterate through an array and returns a new array,
     //This is helpful when creating a new array based on the values of an existing array.
 };
