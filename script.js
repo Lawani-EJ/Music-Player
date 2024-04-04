b94ae873f3dcd75f1cb204939d2b839992639165
elyonjohn <elyonjohn60@gmail.com>
2024-04-05 00:11:39 +0100

    JS Format Formatted JS for better user readability

--- script.js
+++ script.js
@@ -93,7 +93,7 @@ const allSongs = [
     },
 ];
 
-const audio = newAudio(); //The Audio() constructor creates and returns a new HTMLAudioElement which can be either attached to a document for the user to interact with and/or listen to, or can be used offscreen to manage and play audio.
+const audio = new Audio(); //The Audio() constructor creates and returns a new HTMLAudioElement which can be either attached to a document for the user to interact with and/or listen to, or can be used offscreen to manage and play audio.
 let userData = {
     // The music player should be keeping track of the songs,
     //The current song playing,
@@ -104,7 +104,7 @@ let userData = {
     songs: [...allSongs],// This spread array allows me to copy all the elements from one array to another.
     //It can be also used to concatenate through multiple array's into one. 
 
-//To handle the current song's information and tracking its playback time
+    //To handle the current song's information and tracking its playback time
     currentSong: null, // so what this is for is the default state of the play button if there were a bunch of songs in the playlist, now it is null because there is nothing playing and the current time is zero so it starts at the beginning so when we click shuffle or next it goes to zero
     songCurrentTime: 0,// so what this is for is the default state of the play button if there were a bunch of songs in the playlist, now it is null because there is nothing playing and the current time is zero so it starts at the beginning so when we click shuffle or next it goes to zero
 };
@@ -123,12 +123,13 @@ const renderSongs = array => {
         <span class="playlist-song-duration">${song.duration}</span>
         <button class="playlist-song-delete" aria-label ="Delete ${song.title}">
         <svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="8" fill="#4d4d62"/><path fill-rule="evenodd" clip-rule="evenodd" d="M5.32587 5.18571C5.7107 4.90301 6.28333 4.94814 6.60485 5.28651L8 6.75478L9.39515 5.28651C9.71667 4.94814 10.2893 4.90301 10.6741 5.18571C11.059 5.4684 11.1103 5.97188 10.7888 6.31026L9.1832 7.99999L10.7888 9.68974C11.1103 10.0281 11.059 10.5316 10.6741 10.8143C10.2893 11.097 9.71667 11.0519 9.39515 10.7135L8 9.24521L6.60485 10.7135C6.28333 11.0519 5.7107 11.097 5.32587 10.8143C4.94102 10.5316 4.88969 10.0281 5.21121 9.68974L6.8168 7.99999L5.21122 6.31026C4.8897 5.97188 4.94102 5.4684 5.32587 5.18571Z" fill="white"/></svg></button>
-        </button>`//what this does is that it interpolates all the elements that are responsible for displaying the song details
+        </button>
+        </li>`;//what this does is that it interpolates all the elements that are responsible for displaying the song details
     }).join(""); //the join() method is used for concatenating all the elements of an array in to a simple string that's what happened here //The map() method is used to iterate through an array and returns a new array,
     //This is helpful when creating a new array based on the values of an existing array.
-    playlistSongs.innerHTML = songsHTML // this will insert the li element  that i have just created inside the  ul provided in the HTML file
+    playlistSongs.innerHTML = songsHTML;// this will insert the li element  that i have just created inside the  ul provided in the HTML file
 };
 
-renderSongs(userData?.songs); 
+renderSongs(userData?.songs);
 //This will render all the songs  that are stored inside our `userData` object.
 //It helps prevent errors when accesing nested properties that might be null or undefined
