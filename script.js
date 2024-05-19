9ee70f6cfc06c6acff756eee7af1916ec4bcccb9
Lawani-EJ <elyonjohn60@gmail.com>
2024-05-19 19:08:53 +0100

    Play Button Functionality For this commit I've added an esle block, This block calls the playsong function with the id of the current playing song as the argument, it will ensure the currently playing song play when the play button is clciked.

--- script.js
+++ script.js
@@ -151,6 +151,8 @@ const renderSongs = array => {
 playButton.addEventListener("click",() => {
     if(!userData?.currentSong){
         playSong(userData?.songs[0].id)
+    } else {
+        playSong(userData?.currentSong.id);
     }
 })
 
