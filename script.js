aef5ebeecfa205ddf004516be18c686e2ec449f7
Lawani-EJ <elyonjohn60@gmail.com>
2024-06-24 23:05:20 +0100

    Fixed several bugs.

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ const previousButton = document.getElementById("previous");
 const shuffleButton = document.getElementById("shuffle");
 
 const allSongs = [
-    { id: 0, title: "Yam Yam", artist: "No Vacation", duration: "03:36", src: "/Music-Player/Assets/Songs/Yam-Yam.mp3" },
-    { id: 1, title: "Napster", artist: "Jean Dawson", duration: "02:36", src: "/Music-Player/Assets/Songs/Napster.mp3" },
-    { id: 2, title: "Playground", artist: "Steve-Lacy", duration: "03:33", src: "/Music-Player/Assets/Songs/Playground.mp3" },
-    { id: 3, title: "Redbone", artist: "Childish-Gambino", duration: "05:27", src: "/Music-Player/Assets/Songs/Redbone.mp3" },
-    { id: 4, title: "See You Again", artist: "Tyler, The Creator", duration: "03:00", src: "/Music-Player/Assets/Songs/Telephones.mp3" },
-    { id: 5, title: "Nights", artist: "Frank-Ocean", duration: "05:07", src: "/Music-Player/Assets/Songs/Nights.mp3" },
+    { id: 0, title: "Yam Yam", artist: "No Vacation", duration: "03:36", src: "./Assets/Songs/Yam-Yam.mp3" },
+    { id: 1, title: "Napster", artist: "Jean Dawson", duration: "02:36", src: "./Assets/Songs/Napster.mp3" },
+    { id: 2, title: "Playground", artist: "Steve-Lacy", duration: "03:33", src: "./Assets/Songs/Playground.mp3" },
+    { id: 3, title: "Redbone", artist: "Childish-Gambino", duration: "05:27", src: "./Assets/Songs/Redbone.mp3" },
+    { id: 4, title: "See You Again", artist: "Tyler, The Creator", duration: "03:00", src: "./Assets/Songs/See You Again.mp3" },
+    { id: 5, title: "Nights", artist: "Frank-Ocean", duration: "05:07", src: "./Assets/Songs/Pursuit Of Happiness nightmare.mp3" },
     { id: 6, title: "Pursuit Of Happiness", artist: "Kid-Cudi", duration: "04:55", src: "/Music-Player/Assets/Songs/Pursuit Of Happiness nightmare.mp3" },
-    { id: 7, title: "Telephones", artist: "Vacations", duration: "03:32", src: "/Music-Player/Assets/Songs/Telephones.mp3" },
-    { id: 8, title: "Valentina", artist: "Daniel Caesar", duration: "02:34", src: "/Music-Player/Assets/Songs/Valentina.mp3" },
-    { id: 9, title: "Yes I'm Changing", artist: "Tame Impala", duration: "04:31", src: "/Music-Player/Assets/Songs/Yes I_m Changing.mp3" },
+    { id: 7, title: "Telephones", artist: "Vacations", duration: "03:32", src: "./Assets/Songs/Telephones.mp3" },
+    { id: 8, title: "Valentina", artist: "Daniel Caesar", duration: "02:34", src: "./Assets/Songs/Valentina.mp3" },
+    { id: 9, title: "Yes I'm Changing", artist: "Tame Impala", duration: "04:31", src: "./Assets/Songs/Yes I_m Changing.mp3" },
 ];
 
 const audio = new Audio();
